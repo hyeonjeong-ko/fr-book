a39bba9f0ec7f8952fc179bdc7c892f4bb5357a9
Kohyeonjeong <toyu7870@naver.com>
2023-12-19 21:58:11 +0900

    Initial commit

---
+++ src/main/java/com/webservice/frbook/FrBookApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.frbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrBookApplication.class, args);
+    }
+
+}
